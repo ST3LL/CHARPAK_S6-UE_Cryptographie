a23675980503db5a1fe0a912b14dc5cf5400b144
tinou99 <tinou99.f@gmail.com>
2020-01-24 09:25:47 +0100

    Create crypto_1.py

---
+++ Message 1/crypto_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 09:22:57 2020
+
+@author: tinou_99
+"""
+
+import os
+
+with open('nom_du_fichier.txt', 'r') as file:
+    message = file.read() 
