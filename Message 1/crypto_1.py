cea28ac3437502cd9d63f48b7f96814165691591
tinou99 <tinou99.f@gmail.com>
2020-02-07 07:13:59 +0100

    Update crypto_1.py

--- Message 1/crypto_1.py
+++ Message 1/crypto_1.py
@@ -9,3 +9,34 @@ import os
 
 with open('nom_du_fichier.txt', 'r') as file:
     message = file.read() 
+    
+    
+
+def frequences(texte):
+    """
+    Prend en argument une chaîne de caractères et renvoie un dictionnaire 
+    dont les clés sont les caractères présents dans le texte et les valeurs 
+    sont le nombre d'occurences du caractère concerné.
+        
+    Entrée:
+        (str) 
+    Sortie:
+        (dict()) 
+    Test:
+        >>> frequences('bonjour') == {'b': 1, 'j': 1, 'n': 1, 'o': 2, 'r': 1, 'u': 1}
+        True
+        >>> frequences('ABRACADABRA') == {'A': 5, 'B': 2, 'C': 1, 'D': 1, 'R': 2}
+        True
+    """
+    d = dict()
+    for l in texte:
+        if l not in d:
+            d[l] = 1
+        else:
+            d[l] += 1
+    return d
+
+
+
+if __name__ == "__main__":
+    frequences(message)
