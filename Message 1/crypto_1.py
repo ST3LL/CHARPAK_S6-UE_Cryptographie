79916b0ce189311889b162f7ee6e8d3cc78e9f10
tinou99 <tinou99.f@gmail.com>
2020-02-14 10:00:32 +0100

    Update crypto_1.py

--- Message 1/crypto_1.py
+++ Message 1/crypto_1.py
@@ -5,39 +5,10 @@ Created on Fri Jan 24 09:22:57 2020
 @author: tinou_99
 """
 
-import numpy as np
-
 
 with open('message1.txt', 'r') as file:
     message = file.read() 
     
-    
-
-def frequences(texte):
-    """
-    Prend en argument une chaîne de caractères et renvoie un dictionnaire 
-    dont les clés sont les caractères présents dans le texte et les valeurs 
-    sont le nombre d'occurences du caractère concerné.
-        
-    Entrée:
-        (str) 
-    Sortie:
-        (dict()) 
-    Test:
-        >>> frequences('bonjour') == {'b': 1, 'j': 1, 'n': 1, 'o': 2, 'r': 1, 'u': 1}
-        True
-        >>> frequences('ABRACADABRA') == {'A': 5, 'B': 2, 'C': 1, 'D': 1, 'R': 2}
-        True
-    """
-    d = dict()
-    for l in texte:
-        if l not in d:
-            d[l] = 1
-        else:
-            d[l] += 1
-    return d
-
-
 
 def decrypt(texte, nb):
     # tab = [list(texte[k: k + nb]) for k in range(0,len(texte),nb)]
