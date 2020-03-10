6c2ffd4a71dae392cd3d92ad5fbdd7531c9eba72
tinou99 <tinou99.f@gmail.com>
2020-03-10 09:07:35 +0100

    created

---
+++ Message_4/crypto_4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 09:03:00 2020
+
+@author: tinou_99
+"""
+
+with open('message3.txt', 'r') as file:
+    message = file.read() 
+#print(message)
