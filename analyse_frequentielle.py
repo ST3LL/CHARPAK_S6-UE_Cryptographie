4485b6e6fa388d75a99fc2aa418a9f5135f33fef
tinou99 <tinou99.f@gmail.com>
2020-04-04 19:45:29 +0200

    doctest added

--- analyse_frequentielle.py
+++ analyse_frequentielle.py
@@ -4,10 +4,12 @@ Created on Fri Feb 14 09:58:22 2020
 
 @author: tinou
 """
+
 import string 
 
+
 #fréquence avec un dictionnaire
-def frequences(texte):
+def frequence(texte):
     """
     Prend en argument une chaîne de caractères et renvoie un dictionnaire 
     dont les clés sont les caractères présents dans le texte et les valeurs 
@@ -18,9 +20,7 @@ def frequences(texte):
     Sortie:
         (dict()) 
     Test:
-        >>> frequences('bonjour') == {'b': 1, 'j': 1, 'n': 1, 'o': 2, 'r': 1, 'u': 1}
-        True
-        >>> frequences('ABRACADABRA') == {'A': 5, 'B': 2, 'C': 1, 'D': 1, 'R': 2}
+        >>> frequence('bonjour') == {'b': 1, 'j': 1, 'n': 1, 'o': 2, 'r': 1, 'u': 1}
         True
     """
     d = dict()
@@ -35,8 +35,21 @@ def frequences(texte):
 
 #le caractère le plus fréquent
 def caractere_plus_frequent(texte):
+    """
+    Prend en argument une chaîne de caractères et renvoie une liste 
+    représentant le classement de manière décroissante de la fréquence
+    d'apparition des lettres composants la chaine de caractère saisie.
+        
+    Entrée:
+        (str) 
+    Sortie:
+        (list) 
+    Test:
+        >>> mot = "anticonstitutionnellement"
+        >>> caractere_plus_frequent(mot) == ['n', 't', 'i', 'e', 'o', 'l', 'c', 'u', 'a', 'm', 's']
+        True
+    """
     t = list(texte)
-    #new_list = []
     lettres = set(t)
     nb_lettres = sorted(lettres, key = lambda self, other = t: other.count(self), reverse = True)    
     return nb_lettres
@@ -44,22 +57,37 @@ def caractere_plus_frequent(texte):
  
     
 #liste des caractères les plus fréquents
-def frequency(word) :
+def frequence_abc(mot) :
+    """
+    Prend en argument une chaîne de caractères et renvoie un tuple composé 
+    du nombre d'occurance max et une liste des lettres ayant l'occurance 
+    la plus élevée (seulement pour des chaînes de caractères comprenant 
+    les 26 lettres de l'alphabet français).
+        
+    Entrée:
+        (str) 
+    Sortie:
+        (tuple) => (int, list)
+    Test:
+        >>> mot = "anticonstitutionnellement"
+        >>> frequence_abc(mot) == (5, ['n', 't'])
+        True
+    """
     freq = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for letter in word :
-        if letter in string.ascii_lowercase :
-            freq[ord(letter) - ord('a')] += 1
+    for lettre in mot :
+        if lettre in string.ascii_lowercase :
+            freq[ord(lettre) - ord('a')] += 1
                  
     sup = [i for i, j in enumerate(freq) if j == (max(freq))] 
-    new_freq = []
+    n_freq = []
     for k in sup :
-        new_freq.append(string.ascii_lowercase[k]) 
-    return max(freq), new_freq
+        n_freq.append(string.ascii_lowercase[k]) 
+    return max(freq), n_freq
 
 
 
 
 if __name__ == "__main__":
     #print(caractere_plus_frequent("anticonstitutionnellement"))
-    print(frequency("anticonstitutionnellement"))
+    print(frequence_abc("anticonstitutionnellement"))
     
