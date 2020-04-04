542809c2f83527f1289db550264d88f956e0d872
tinou99 <tinou99.f@gmail.com>
2020-04-04 21:19:17 +0200

    d_vigenere function created

--- Message_5/crypto_5.py
+++ Message_5/crypto_5.py
@@ -14,19 +14,31 @@ with open('message5.txt', 'r') as file:
 
 
 #déchiffrage vigenère pour 26 lettres alphabet français 
-def d_vigenere_abc(message,key) :
-    cypher=[]
-    for i,letter in enumerate(message) :
-        if letter in string.ascii_lowercase :
-            letter = ord(letter) - ord(key[i % len(key)])
-            letter = chr(letter % 26 + ord('a'))            
-        cypher.append(letter)
-    return "".join(cypher)
+def d_vigenere_abc(message,cle) :
+    c=[]
+    for i,lettre in enumerate(message) :
+        if lettre in string.ascii_lowercase :
+            lettre = ord(lettre) - ord(cle[i % len(cle)])
+            lettre = chr(lettre % 26 + ord('a'))            
+        c.append(lettre)
+    return "".join(c)
+
+
+
+def d_vigenere(message, cle) :
+    n_msg = []
+    n_cle = [ord(e) for e in cle]
+    msg = [ord(k) for k in message]
+    for i in range(len(msg)) :
+        msg[i] -= int(n_cle[i % len(n_cle)])
+        print(msg[i])
+        n_msg.append(chr(msg[i]))        
+    return "".join(n_msg)
 
 
  
 if __name__ == "__main__":
     message = "dhkmjcmhvwiilrpzi"
-    key = "bachelierbachelie"
-    print(d_vigenere_abc(message, key))
-    
+    cle = "bachelierbachelie"
+    #print(d_vigenere_abc(message, key))
+    print(d_vigenere(message,cle))
