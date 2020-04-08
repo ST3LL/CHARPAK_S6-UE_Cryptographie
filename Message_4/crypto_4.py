61e8b583f68e89d5c47c64cb7a574de23f2b03c7
tinou99 <tinou99.f@gmail.com>
2020-04-08 15:07:20 +0200

    error fixed

--- Message_4/crypto_4.py
+++ Message_4/crypto_4.py
@@ -6,7 +6,7 @@ Created on Fri Feb 14 08:53:46 2020
 """
 
 
-with open('message3.txt', 'r') as file:
+with open('message4.txt', 'r') as file:
     message = file.read() 
 #print(message)
 
@@ -21,36 +21,62 @@ def frequences(texte):
     return d
 
 
+
+def separateur(message):
+    pair = []
+    impair = []
+    for i in range(len(message)):
+        if i % 2 == 0:
+            pair.append(message[i])
+        else:
+            impair.append(message[i])
+    return pair, impair
+
+
+
+def colleur(pair, impair):
+    n_msg = []
+    for i in range(len(pair)):
+        n_msg.append(pair[i])
+        n_msg.append(impair[i])
+    return ''.join(n_msg)
+
+
+
 def cle_probable(message):
-    for i in enumerate(message):
-        carac = frequences(message)
-        pcarac_max = (max([(carac[e], e) for e in carac], key= lambda x : x[0]) if i % 2 == 0)
-        cle_pair = ord(pcarac_max[1]) - ord(' ') 
-        icarac_max = max([(icarac[e], e) for e in icarac], key= lambda x : x[0])
-        cle_impair = ord(icarac_max[1]) - ord(' ') 
-    return cle_pair, cle_impair
+    carac = frequences(message)
+    carac_max = max([(carac[e], e) for e in carac], key= lambda x : x[0])
+    cle = ord(carac_max[1]) - ord(' ') 
+    return cle
 
 
-def cesar_chiffrage(nb, decalage):
-    return (ord(nb) + decalage)
-    
-def cesar_dechiffrage(nb, decalage):
-    return (ord(nb) - decalage)
-    
 
-def cesar(message, cle_pair, cle_impair):
-    new_message = []
-    cle_pair = cle_probable[0]
-    cle_impair = cle_probable[1]
-    for i, l in enumerate(message):
-        crypt = chr(cesar_dechiffrage(l, cle_pair if i % 2 == 0 else cle_impair))
-        new_message.append(crypt)
-    new = "".join(new_message)
+def cesar_dechiffrage(lettre, cle):
+    return (ord(lettre) - cle)
+
+
+#c'est très très moche
+def cesar(message):
+    n_msg_p = []
+    n_msg_i = []
+    m = separateur(message)
+    pair = m[0]
+    impair = m[1]
+    p_cle = cle_probable("".join(pair))
+    i_cle = cle_probable("".join(impair))
+    for l in pair:
+        p_crypt = chr(cesar_dechiffrage(l, p_cle))
+        n_msg_p.append(p_crypt)
+    for e in impair :
+        i_crypt = chr(cesar_dechiffrage(l, i_cle))
+        n_msg_i.append(i_crypt)
+    new = colleur(n_msg_p, n_msg_i)
     return new
 
 
+
 if __name__ == "__main__":
-    print(cesar(message, cle_probable(message)))
+    print(cesar(message))
     
 
     
--- Message_5/crypto_5.py
+++ Message_5/crypto_5.py
@@ -25,20 +25,92 @@ def d_vigenere_abc(message,cle) :
 
 
 
+
+
+def frequences(texte):
+    d = dict()
+    for l in texte:
+        if l not in d:
+            d[l] = 1
+        else:
+            d[l] += 1
+    return d
+
+
+
+def cle_probable(message):
+    n_carac = colleur(message)
+    carac = frequences(n_carac)
+    carac_max = max([(carac[e], e) for e in carac], key= lambda x : x[0])
+    cle = ord(carac_max[1]) - ord(' ') 
+    return cle
+
+
+
+def cesar_dechiffrage(lettre, cle):
+    return (ord(lettre) + cle)
+    
+    
+
+def cesar(message, cle):
+    new_message = []
+    m = list(message)
+    for l in m:
+        crypt = chr(cesar_dechiffrage(l, cle))
+        new_message.append(crypt)
+    new = "".join(new_message)
+    return new
+
+
+def separateur(message, l_cle):
+    sep = []
+    for c in range(l_cle):
+        sous_sep = []
+        for e in range(c, len(message), l_cle): #pas de longueur de la clé
+            sous_sep.append(message[e])
+        sep.append(sous_sep)
+    return sep
+
+
+
+def colleur(v_msg):
+    n_msg = []
+    for c in range(len(v_msg)):
+        for e in range(len(v_msg[c])):
+            n_msg.append(v_msg[c])
+    return ''.join(n_msg)
+
+
+
+def d_vigenere(message, l_cle):
+    n_msg = []
+    sep = separateur(message, l_cle) 
+    for c in sep :
+        cle = cle_probable(sep[c])
+        n_msg.append(cesar(c, cle))
+    return colleur(n_msg)
+
+
+
+
+"""
 def d_vigenere(message, cle) :
     n_msg = []
     n_cle = [ord(e) for e in cle]
     msg = [ord(k) for k in message]
-    for i in range(len(msg)) :
+    for i in message :
         msg[i] -= int(n_cle[i % len(n_cle)])
-        print(msg[i])
-        n_msg.append(chr(msg[i]))        
+        #print(msg[i])
+        n_msg.append(chr(msg[i]))  
+        sup = cle_probable(message)
+        n_msg = cesar(message, sup)    
     return "".join(n_msg)
+"""
 
 
  
 if __name__ == "__main__":
     message = "dhkmjcmhvwiilrpzi"
-    cle = "bachelierbachelie"
+    l_cle = 3
     #print(d_vigenere_abc(message, key))
-    print(d_vigenere(message,cle))
+    print(d_vigenere(message,l_cle))
