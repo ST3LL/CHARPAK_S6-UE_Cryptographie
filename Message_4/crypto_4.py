2ccb3ca502dbe9a7b9231d790ad18945564a499e
tinou99 <tinou99.f@gmail.com>
2020-04-21 15:20:55 +0200

    done

--- Message_4/crypto_4.py
+++ Message_4/crypto_4.py
@@ -8,7 +8,7 @@ Created on Fri Feb 14 08:53:46 2020
 
 with open('message4.txt', 'r') as file:
     message = file.read() 
-#print(message)
+
 
 
 def frequences(texte):
@@ -22,29 +22,8 @@ def frequences(texte):
 
 
 
-def separateur(message):
-    pair = []
-    impair = []
-    for i in range(len(message)):
-        if i % 2 == 0:
-            pair.append(message[i])
-        else:
-            impair.append(message[i])
-    return pair, impair
-
-
-
-def colleur(pair, impair):
-    n_msg = []
-    for i in range(len(pair)):
-        n_msg.append(pair[i])
-        n_msg.append(impair[i])
-    return ''.join(n_msg)
-
-
-
-def cle_probable(message):
-    carac = frequences(message)
+def cle_probable(n_carac):
+    carac = frequences(n_carac)
     carac_max = max([(carac[e], e) for e in carac], key= lambda x : x[0])
     cle = ord(carac_max[1]) - ord(' ') 
     return cle
@@ -53,31 +32,74 @@ def cle_probable(message):
 
 def cesar_dechiffrage(lettre, cle):
     return (ord(lettre) - cle)
+    
+    
 
-
-#c'est très très moche
-def cesar(message):
-    n_msg_p = []
-    n_msg_i = []
-    m = separateur(message)
-    pair = m[0]
-    impair = m[1]
-    p_cle = cle_probable("".join(pair))
-    i_cle = cle_probable("".join(impair))
-    for l in pair:
-        p_crypt = chr(cesar_dechiffrage(l, p_cle))
-        n_msg_p.append(p_crypt)
-    for e in impair :
-        i_crypt = chr(cesar_dechiffrage(l, i_cle))
-        n_msg_i.append(i_crypt)
-    new = colleur(n_msg_p, n_msg_i)
+def cesar(message, cle):
+    new_message = []
+    m = list(message)
+    for l in m:
+        crypt = chr(cesar_dechiffrage(l, cle))
+        new_message.append(crypt)
+    new = "".join(new_message)
     return new
 
 
 
-if __name__ == "__main__":
-    print(cesar(message))
+def auto_cesar(message):
+    pair, impair = separateur(message)
+    return colleur((cesar(pair, cle_probable(pair)), cesar(impair, cle_probable(impair))))
+
+
+
+def separateur(message):
+    pair = []
+    impair = []
+    for c in range(len(message)):
+        if c % 2 == 0:
+            pair.append(message[c])
+        else:
+            impair.append(message[c])
+    return pair, impair
+
+
+"""
+def plus_longue_liste(m):
+    n_m = 0
+    for i in m:
+        if len(m[i]) < len(m[i+1]):
+            n_m = len(m[i+1])
+        else:
+            n_m = len(m[i])
+    return n_m
     
+ """           
+
+
+def colleur(v_msg):
+    n_msg = []
+    for c in range(len(v_msg[0])):
+        for e in range(len(v_msg[0])):
+            try : 
+                n_msg.append(v_msg[e][c])
+            except : 
+                pass
+    return ''.join(n_msg)
+
+
+
+"""
+def vigenere(text, key):
+    text = [i for i in text]
+    encrypted = [cesar(text[i], key[i]) for i in range(len(text))]
+    return "".join(encrypted)
+"""
+
+
+if __name__ == "__main__" :
+    print(auto_cesar(message))
+    #print(plus_longue_liste([['a', 'a', 'a'], ['b', 'b'], ["c","c","c","c"]]))
+    #print(colleur([['a', 'a', 'a'], ['b', 'b']]))
 
     
     
